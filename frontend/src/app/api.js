b7de854aa354f07e9aec1a928f3bfdcb7874526a
zainikhan999 <zk.032918@gmail.com>
2025-11-29 11:57:32 +0500

    Deleted extra folders from frontend

--- frontend/src/app/api.js
+++
@@ -1,9 +0,0 @@
-import Axios from "axios";
-import { getAuthToken } from "./authorization";
-
-const api = Axios.create({
-  baseURL: "http://api/users",
-  timeout: 10000,
-});
-
-export default api;
